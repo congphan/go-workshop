6477e5b1fac8c9e583ac9b427cc55671a0dfaf1e
congphan <phan.thanh.cong@moneyforward.vn>
2025-10-03 13:39:48 +0700

    custom type for context key

--- good/primative/calc.go
+++ good/primative/calc.go
@@ -15,24 +15,28 @@ func QueryTenantUser(tenantUID TenantUID, tenantUserUID TenantUserUID) int {
 	return int(tenantUID) - int(tenantUserUID) // minus for demonstration purposes
 }
 
+type (
+	contextKey string
+)
+
 const (
-	tenantUIDKey string = "tenant_uid"
-	userIDKey    string = "user_id"
+	contextKeyTenantUID contextKey = "tenant_uid"
+	contextKeyUserID    contextKey = "user_id"
 )
 
 func InitContextLoggger() context.Context {
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, tenantUIDKey, "default-tenant-uid")
-	ctx = context.WithValue(ctx, userIDKey, "default-user-id")
+	ctx = context.WithValue(ctx, contextKeyTenantUID, "default-tenant-uid")
+	ctx = context.WithValue(ctx, contextKeyUserID, "default-user-id")
 
 	return ctx
 }
 
 func PrintLogFromContext(ctx context.Context, message string) {
-	tenantUID := ctx.Value(tenantUIDKey)
-	userID := ctx.Value(userIDKey)
+	tenantUID := ctx.Value(contextKeyTenantUID)
+	userID := ctx.Value(contextKeyUserID)
 
 	// Simulate logging with tenant and user context
-	logMessage := fmt.Sprintf("%s: %v, %s: %v - ", tenantUIDKey, tenantUID, userIDKey, userID) + message
+	logMessage := fmt.Sprintf("%s: %v, %s: %v - ", contextKeyTenantUID, tenantUID, contextKeyUserID, userID) + message
 	fmt.Println(logMessage)
 }
